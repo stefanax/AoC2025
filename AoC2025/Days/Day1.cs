9aed188a89822adf7283e32d73e095a72d348ee6
Stefan Axelsson <stefan.axelsson@bejoynd.com>
2025-12-01 07:52:36 +0100

    Start of Day One

--- AoC2025/Days/Day1.cs
+++ AoC2025/Days/Day1.cs
@@ -8,25 +8,8 @@ public class Day1
     {
         var input = _inputFiles.ReadInputFileForDay(1, false);
         var inputList = _inputFiles.SplitString(input);
-        
-        var listOne = new List<int>();
-        var listTwo = new List<int>();
 
-        foreach (var inputString in inputList)
-        {
-            var inputValues = inputString.Split("   ");
-            listOne.Add(int.Parse(inputValues[0]));
-            listTwo.Add(int.Parse(inputValues[1]));
-        }
-        
-        listOne.Sort();
-        listTwo.Sort();
-
-        var result = 0;
-        for (int i = 0; i < listOne.Count; i++)
-        {
-            result += int.Abs(listOne[i] - listTwo[i]);
-        }
+        var result = "temp value";
         
         Console.WriteLine($"Step one result: {result}");
     }
@@ -36,31 +19,7 @@ public class Day1
         var input = _inputFiles.ReadInputFileForDay(1, false);
         var inputList = _inputFiles.SplitString(input);
         
-        var listOne = new List<int>();
-        var listTwo = new List<int>();
-
-        foreach (var inputString in inputList)
-        {
-            var inputValues = inputString.Split("   ");
-            listOne.Add(int.Parse(inputValues[0]));
-            listTwo.Add(int.Parse(inputValues[1]));
-        }
-
-        // Yes, this is probably "not the best way" but...meh.
-        var result = 0;
-        for (int i = 0; i < listOne.Count; i++)
-        {
-            var multiplicator = 0;
-            for (int j = 0; j < listTwo.Count; j++)
-            {
-                if (listOne[i] == listTwo[j])
-                {
-                    multiplicator++;
-                }
-            }
-            
-            result += listOne[i] * multiplicator;
-        }
+        var result = "temp value";
         
         Console.WriteLine($"Step two result: {result}");
     }
--- AoC2025/Program.cs
+++ AoC2025/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var day = new Day13();
+            var day = new Day1();
             day.Step1();
             day.Step2();
         }
