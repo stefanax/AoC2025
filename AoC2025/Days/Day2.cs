640742824e06dbe3ecc192c30047fc5926aa0d07
stefanax <40429241+stefanax@users.noreply.github.com>
2025-12-02 10:08:11 +0100

    Handle comma-separated ranges for Day2 step1

--- AoC2025/Days/Day2.cs
+++ AoC2025/Days/Day2.cs
@@ -7,10 +7,58 @@ public class Day2
     public void Step1()
     {
         var input = _inputFiles.ReadInputFileForDay(2, false);
-        var inputList = _inputFiles.SplitString(input);
+        var rangeStrings = input
+            .Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
 
-        // TODO: Implement Day 2 Step 1 solution
-        Console.WriteLine($"Step one result: {0}");
+        long matchingSum = 0;
+
+        foreach (var range in rangeStrings)
+        {
+            var rangeParts = range.Split('-', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+            if (rangeParts.Length != 2
+                || !long.TryParse(rangeParts[0], out var start)
+                || !long.TryParse(rangeParts[1], out var end))
+            {
+                continue;
+            }
+
+            if (start > end)
+            {
+                (start, end) = (end, start);
+            }
+
+            for (var value = start; value <= end; value++)
+            {
+                if (IsMatchingPattern(value))
+                {
+                    matchingSum += value;
+                }
+            }
+        }
+
+        Console.WriteLine($"Step one result: {matchingSum}");
+    }
+
+    private static bool IsMatchingPattern(long value)
+    {
+        if (value < 0)
+        {
+            return false;
+        }
+
+        var valueString = value.ToString();
+
+        if (valueString.Length % 2 != 0)
+        {
+            return false;
+        }
+
+        var halfLength = valueString.Length / 2;
+        var firstHalf = valueString[..halfLength];
+        var secondHalf = valueString[halfLength..];
+
+        return firstHalf == secondHalf;
     }
 
     public void Step2()
