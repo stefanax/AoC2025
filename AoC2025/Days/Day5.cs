d6811792bd4a484a176b95fe27951a0f24e4d3bb
stefanax <40429241+stefanax@users.noreply.github.com>
2025-12-08 08:44:24 +0100

    Add Day5 stub and inputs

---
+++ AoC2025/Days/Day5.cs
@@ -0,0 +1,22 @@
+namespace AoC2024;
+
+public class Day5
+{
+    private readonly InputFiles _inputFiles = new InputFiles();
+
+    public void Step1()
+    {
+        var input = _inputFiles.ReadInputFileForDay(5, false);
+        var inputList = _inputFiles.SplitString(input);
+
+        Console.WriteLine("Step one result: (not implemented)");
+    }
+
+    public void Step2()
+    {
+        var input = _inputFiles.ReadInputFileForDay(5, false);
+        var inputList = _inputFiles.SplitString(input);
+
+        Console.WriteLine("Step two result: (not implemented)");
+    }
+}
