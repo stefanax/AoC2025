430895cf0736da0df9b89283d54776a14a6a5535
stefanax <40429241+stefanax@users.noreply.github.com>
2025-12-11 09:39:15 +0100

    Add Day6 scaffolding and input placeholders

---
+++ AoC2025/Days/Day6.cs
@@ -0,0 +1,22 @@
+namespace AoC2024;
+
+public class Day6
+{
+    private readonly InputFiles _inputFiles = new InputFiles();
+
+    public void Step1()
+    {
+        var input = _inputFiles.ReadInputFileForDay(6, false);
+        var inputList = _inputFiles.SplitString(input);
+
+        Console.WriteLine("Step one result:");
+    }
+
+    public void Step2()
+    {
+        var input = _inputFiles.ReadInputFileForDay(6, false);
+        var inputList = _inputFiles.SplitString(input);
+
+        Console.WriteLine("Step two result:");
+    }
+}
